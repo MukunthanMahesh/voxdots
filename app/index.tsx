bbe6bd3f449b3786570705ff0679b86449799a8a
Mukunthan Mahesh <mukunthanmahesh@gmail.com>
2026-02-21 01:50:22 -0500

    Show generations in index and add region screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,73 +1,51 @@
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
-interface Pokemon {
+interface Generations {
   name: string;
-  image: string;
-  imageBack: string;
-  types: PokemonType[];
+  regionName: string;
 }
 
-interface PokemonType {
-  type: {
-    name: string;
-    url: string;
-  };
-}
-
-const colorsByType = {
-  normal: "#A8A77A",
-  fire: "#EE8130",
-  water: "#6390F0",
-  electric: "#F7D02C",
-  grass: "#7AC74C",
-  ice: "#96D9D6",
-  fighting: "#C22E28",
-  poison: "#A33EA1",
-  ground: "#E2BF65",
-  flying: "#A98FF3",
-  psychic: "#F95587",
-  bug: "#A6B91A",
-  rock: "#B6A136",
-  ghost: "#735797",
-  dragon: "#6F35FC",
-  dark: "#705746",
-  steel: "#B7B7CE",
-  fairy: "#D685AD",
+const bannersByRegion = {
+  kanto: "#A8A77A", // muted olive
+  johto: "#6FA8DC", // soft blue
+  hoenn: "#F6B26B", // warm orange
+  sinnoh: "#8E7CC3", // lavender
+  unova: "#93C47D", // fresh green
+  kalos: "#E06666", // coral red
+  alola: "#FFD966", // sunny yellow
+  galar: "#76A5AF", // teal
+  paldea: "#C27BA0", // rose
 };
 
 export default function Index() {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [generations, setGenerations] = useState<Generations[]>([]);
 
-  console.log(JSON.stringify(pokemons[0], null, 2));
   useEffect(() => {
-    // fetch pokemon data
-    fetchPokemons();
+    // Fetch Generations (GET Endpoint: https://pokeapi.co/api/v2/generation/{id or name}/)
+    fetchGenerations();
   }, []);
 
-  async function fetchPokemons() {
+  async function fetchGenerations() {
     try {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0",
-      );
+      const response = await fetch("https://pokeapi.co/api/v2/generation/");
       const data = await response.json();
 
-      // Fethc detailed info for each pokemon in parallel
-      const detailedPokemons = await Promise.all(
-        data.results.map(async (pokemon: any) => {
-          const res = await fetch(pokemon.url);
+      // Fetch Detailed Information of each Generation in Parallel
+      const generationInformation = await Promise.all(
+        data.results.map(async (generation: any) => {
+          const res = await fetch(generation.url);
           const details = await res.json();
           return {
-            name: pokemon.name,
-            image: details.sprites.front_default, // main sprite
-            imageBack: details.sprites.back_default,
-            types: details.types,
+            name: details.name,
+            regionName: details.main_region.name,
           };
         }),
       );
-      // save data in state variable
-      setPokemons(detailedPokemons);
+
+      // Save data in state variable
+      setGenerations(generationInformation);
     } catch (e) {
       console.log(e);
     }
@@ -79,35 +57,19 @@ export default function Index() {
         padding: 16,
       }}
     >
-      {pokemons.map((pokemon) => (
+      {generations.map((generation) => (
         <Link
-          key={pokemon.name}
-          href={{ pathname: "/details", params: { name: pokemon.name } }}
+          key={generation.regionName}
+          href={{ pathname: "/region", params: { name: generation.name } }}
           style={{
             // @ts-ignore
-            backgroundColor: colorsByType[pokemon.types[0].type.name] + 50,
+            backgroundColor: bannersByRegion[generation.regionName],
             padding: 20,
             borderRadius: 20,
           }}
         >
           <View>
-            <Text style={styles.name}>{pokemon.name}</Text>
-            <Text style={styles.type}>{pokemon.types[0].type.name}</Text>
-
-            <View
-              style={{
-                flexDirection: "row",
-              }}
-            >
-              <Image
-                source={{ uri: pokemon.image }}
-                style={{ width: 150, height: 150 }}
-              />
-              <Image
-                source={{ uri: pokemon.imageBack }}
-                style={{ width: 150, height: 150 }}
-              />
-            </View>
+            <Text style={styles.name}>{generation.name}</Text>
           </View>
         </Link>
       ))}
